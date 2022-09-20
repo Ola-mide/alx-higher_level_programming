c02556f57e28e227587e525d0833c8f741805241
Abdulrahman Oyetade <abdulrahmanoyetade@gmail.com>
2022-09-20 05:45:54 -0700

    showing whether the integer is positive or negative

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10,10)
+number = random.randint(-10, 10)
 if number > 0:
-    print(f"{number:d} is positive")
+    print(f"{number} is positive")
 elif number == 0:
-    print(f"{number:d} is zero")
+    print(f"{number} is zero")
 elif number < 0:
-    print(f"{number:d} is negative")
+    print(f"{number} is negative")
