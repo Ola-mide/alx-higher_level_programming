7f1681dd2b35985b84d8a68721b6f4fdb91063cf
Abdulrahman Oyetade <abdulrahmanoyetade@gmail.com>
2022-09-22 04:55:39 -0700

    a program that imports a function from another file and prints the result

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-import add_0
-a = 1
-b = 2
-print("{0} + {1} = {2}".format(a, b, add_0.add(a, b)))
+if __name__ == "__main__":
+    import add_0
+    a = 1
+    b = 2
+    print("{0} + {1} = {2}".format(a, b, add_0.add(a, b)))
