8d8cbcec9bd665f26274d1ff76eb200abab449f9
Abdulrahman Oyetade <abdulrahmanoyetade@gmail.com>
2022-10-11 07:37:59 -0700

    a class Square that defines a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module for a square
+"""
+
+
+class Square:
+    """
+    Defining a square
+
+    Attributes:
+        size (int): The size of the square
+    """
+
+    def __init__(self, size):
+        """
+        The __init__ methos initializes the object
+
+        Args:
+            size (int): The size of the square
+        """
+        self.__size = size
