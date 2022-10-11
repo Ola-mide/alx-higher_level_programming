c347f74ed8c583b8cb6487e4735445402247227b
Abdulrahman Oyetade <abdulrahmanoyetade@gmail.com>
2022-10-11 07:09:03 -0700

    fixing BaseException

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as err:
+    except BaseException as err:
         print("Exception: {}".format(err), file=sys.stderr)
         return False
