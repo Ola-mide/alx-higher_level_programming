cb52d7113e3deb53c85e098a2d1a52e111cdd6c8
Abdulrahman Oyetade <abdulrahmanoyetade@gmail.com>
2022-10-11 07:09:45 -0700

    fixing BaseException

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,7 +4,6 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        fct(*args)
         return fct(*args)
     except BaseException as err:
         print("Exception: {}".format(err), file=sys.stderr)
