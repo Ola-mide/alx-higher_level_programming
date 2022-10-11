b7f7cb982ff846e4b959e0ba5151377153c40e06
Abdulrahman Oyetade <abdulrahmanoyetade@gmail.com>
2022-10-11 09:49:22 -0700

    class Square that defines a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -83,8 +83,10 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif type(value[0]) != int and type(value[1]) != int:
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif value[0] < 0 or value[1] < 0:
+        elif len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers") 
         else:
             self.__position = value
 
@@ -106,11 +108,9 @@ class Square:
         if self.__size == 0:
             print("")
         else:
-            for a in range(self.__position[1]):
-                print("")
+            print("\n"*self.__position[1], end="")
             for i in range(self.__size):
-                for b in range(self.__position[0]):
-                    print(" ", end="")
+                print(" "*self.__position[0], end="")
                 for j in range(self.__size):
                     print("#", end="")
                 print("\n", end="")
