5ad01c06ea07c26d75812005b553b11e81e43764
Abdulrahman Oyetade <abdulrahmanoyetade@gmail.com>
2022-10-18 11:18:20 -0700

    fixing bugs

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -22,8 +22,8 @@ class Rectangle:
             width (int): The width of the rectangle
             height (int): The height of the rectangle
         """
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
