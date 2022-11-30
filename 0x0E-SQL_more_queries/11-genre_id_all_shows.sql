6cc6e25d9c0124fe6aef7703ab4b3dd79a5ee32c
Ola-mide <abdulrahmanoyetade@gmail.com>
2022-11-30 21:15:07 +0100

    a script that lists all shows contained in the database hbtn_0d_tvshows

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,4 @@
--- a script that lists all shows contained in the database hbtn_0d_tvshows
+-- a script that lists all shows contained in the database hbtn_0d_tvshows 
 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
