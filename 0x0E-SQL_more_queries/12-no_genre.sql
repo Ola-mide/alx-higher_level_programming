3c4b89a08737074af368b5c9a7d8929ff7eb6e58
Ola-mide <abdulrahmanoyetade@gmail.com>
2022-11-30 21:15:21 +0100

    a script that lists all shows contained in hbtn_0d_tvshows without a genre linked

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,4 +1,4 @@
--- a script that lists all shows contained in hbtn_0d_tvshows without a genre linked
+-- a script that lists all shows contained in hbtn_0d_tvshows without a genre linked 
 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
