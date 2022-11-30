ded377c1640986c2bb41511cba1cfda2e0138c09
Ola-mide <abdulrahmanoyetade@gmail.com>
2022-11-30 21:14:50 +0100

    a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- a script that lists all shows contained in hbtn_0d_tvshows
--- that have at least one genre linked
+-- that have at least one genre linked 
 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
