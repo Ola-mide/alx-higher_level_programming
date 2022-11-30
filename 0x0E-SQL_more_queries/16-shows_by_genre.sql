f4692cb822fc03b3d12bef22da37796fe823b649
Ola-mide <abdulrahmanoyetade@gmail.com>
2022-11-30 21:16:21 +0100

    a script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,4 +1,4 @@
--- a script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
+-- a script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows 
 
 SELECT tv_shows.title AS title,
 tv_genres.name AS name
