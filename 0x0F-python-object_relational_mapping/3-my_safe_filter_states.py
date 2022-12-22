aee81a43843d6ae035094c5c73e017e30e1ef275
Ola-mide <abdulrahmanoyetade@gmail.com>
2022-12-22 23:02:21 +0100

    a script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument and is safe from MySQL injections

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,8 +9,8 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(
             host="localhost",
-            user="root",
-            passwd="Olamideola-22",
+            user=sys.argv[1],
+            passwd=sys.argv[2],
             db=sys.argv[3],
             port=3306
             )
