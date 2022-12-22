1c6ae22230b78f7a60a2de26e07b1e4a14581f39
Ola-mide <abdulrahmanoyetade@gmail.com>
2022-12-22 15:32:51 +0100

    a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+A module that lists all states from a specific database
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3306
+            )
+    cursor = db.cursor()
+    cursor.execute("SELECT * from states ORDER BY states.id ASC")
+    for row in cursor.fetchall():
+        if row[1] == sys.argv[4]:
+            print("({}, \'{}\')".format(row[0], sys.argv[4]))
+    db.close()
