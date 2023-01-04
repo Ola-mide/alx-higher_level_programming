4f433632495ac3f97a2ecc7c3a55a568d92ca6e3
Ola-mide <abdulrahmanoyetade@gmail.com>
2023-01-04 22:33:44 +0100

    a script that defines creates the state table and its relationship with city

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+A module that contains a State class and an instance Base
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    A class that links to a table and creates columns using class attributes
+
+    Attr:
+        id (int): column for id
+        name (str): column for state names
+    """
+
+    __tablename__ = 'states'
+    id = Column(
+            Integer,
+            primary_key=True,
+            unique=True,
+            autoincrement=True,
+            nullable=False
+            )
+    name = Column(
+            String(128),
+            nullable=False
+            )
+    cities = relationship("City", backref="state", cascade="all, delete")
